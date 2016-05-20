e613a51f395fc8e6fc85d0b66a1d830d21c2aa39
root <jibusss50@gmail.com>
2016-05-20 20:22:31 +0200

    v0.1.28

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,8 +6,8 @@ seetodoCtrl.controller('MainCtrl', function (ionicMaterialInk) {
 
 	ionicMaterialInk.displayEffect();
 
-  var fab = document.getElementById('fab_seemy');
-  fab.addEventListener('click', function() {
-      location.href = 'https://twitter.com/ZachFitzgerald';
-  });
+	var fab = document.getElementById('fab_seemy');
+	fab.addEventListener('click', function () {
+		location.href = 'https://twitter.com/ZachFitzgerald';
+	});
 });
