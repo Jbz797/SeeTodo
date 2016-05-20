38abbeef394061f957fed3cc680048057b2ede6b
root <jibusss50@gmail.com>
2016-05-20 20:21:41 +0200

    v0.1.27

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,5 +3,11 @@
 var seetodoCtrl = angular.module('seetodo.controllers', []);
 
 seetodoCtrl.controller('MainCtrl', function (ionicMaterialInk) {
+
 	ionicMaterialInk.displayEffect();
+
+  var fab = document.getElementById('fab_seemy');
+  fab.addEventListener('click', function() {
+      location.href = 'https://twitter.com/ZachFitzgerald';
+  });
 });
