8fae94557e5f5ded850c0ad5006dbeb8286b9209
root <jibusss50@gmail.com>
2016-05-21 18:48:26 +0200

    v0.3.46

--- www/js/services.js
+++ www/js/services.js
@@ -11,9 +11,10 @@ angular.module('seetodo')
 			$localForage.setItem(todo)
 				.then(function () {
 					console.log("Tâche ajoutée en base");
-					$localForage.getItem('myName').then(function(data) {
-		var myName = data;
-});
+					$localForage.getItem('myName')
+						.then(function (data) {
+							console.log('data');
+						});
 				});
 			return deferred.promise;
 		}
