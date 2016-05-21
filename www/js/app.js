8f4a4116922e40d7cc09472b2f36bcc85d97ba75
root <jibusss50@gmail.com>
2016-05-21 18:39:16 +0200

    v0.3.36

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('seetodo', ['ionic', 'ionic-material'])
+angular.module('seetodo', ['ionic', 'ionic-material', 'LocalForageModule'])
 
 .run(function ($ionicPlatform) {
 	$ionicPlatform.ready(function () {
