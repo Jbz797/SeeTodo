26137bb518dfa2d5d2b681fa422f5f359433c4f1
root <jibusss50@gmail.com>
2016-05-21 17:01:39 +0200

    v0.2.98

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,18 @@
+"use strict";
 
+angular.module('seetodo')
+
+.factory('storage', function ($q) {
+
+	return {
+
+		add: function (todo) {
+			/*var deferred = $q.defer();
+			$scope.todos.push(todo);
+			localStorage.setItem("1", JSON.stringify(todo));
+			deferred.resolve($scope.todos);
+			return deferred.promise;*/
+		}
+
+	};
+});
