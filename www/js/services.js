2a3a28702c02a4f1024f207e813b229cf7a0a574
root <jibusss50@gmail.com>
2016-05-21 19:18:26 +0200

    v0.3.62

--- www/js/services.js
+++ www/js/services.js
@@ -2,23 +2,23 @@
 
 angular.module('seetodo')
 
-.factory('storage', function ($localForage) {
+.factory('storage', function ($localForage, $q) {
 
 	return {
 
 		add: function (todo) {
-			var response = "Erreur lors de l'ajout de la tâche";
+			var deferred = $q.defer();
 			$localForage.setItem(todo.title, 'test')
 				.then(function () {
 					$localForage.getItem(todo.title)
 						.then(function (data) {
 							if(true) {
 								data = 'test';
-								response = "Tâche ajoutée en base";
+								console.log("Tâche ajoutée en base");
 							}
 						});
 				});
-			return response;
+			return deferred.promise;
 		}
 
 	};
