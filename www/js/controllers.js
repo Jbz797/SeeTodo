4f6249d4a8be75e2b20f37c1ae9ec910081e0c57
root <jibusss50@gmail.com>
2016-05-21 20:38:19 +0200

    v0.3.99

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,7 +19,7 @@ angular.module('seetodo')
 	});
 
 	$scope.newTodo = {};
-	$scope.todos = storage.get_todos();
+	$scope.main.todos = storage.get_todos();
 
 	$scope.addTodo = function () {
 		var newTodo = $scope.newTodo;
