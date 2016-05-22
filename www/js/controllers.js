256ffdbb4c0e3adbfdebf2e5ec68a2921ec09c29
root <jibusss50@gmail.com>
2016-05-22 12:05:01 +0200

    v0.4.49

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,7 +34,7 @@ angular.module('seetodo')
 			storage.add(newTodo)
 				.then(function success(response) {
 					$scope.newTodo = {};
-					console.log($scope.todos.title);
+					console.log($scope.todos);
 				});
 		}
 	};
