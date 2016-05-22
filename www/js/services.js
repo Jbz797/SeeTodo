dc127f043f2a224b88e5437c543ebf4d798a1df0
root <jibusss50@gmail.com>
2016-05-22 20:49:24 +0200

    v0.5.72

--- www/js/services.js
+++ www/js/services.js
@@ -18,7 +18,7 @@ angular.module('seetodo')
 				title: todo.title
 			};
 			deferred.resolve(
-				$localForage.setItem(todo.title, this_todo)
+				$localForage.setItem(todo.id, this_todo)
 				.then(function () {
 					console.log('SeeTodo -> Tâche "' + todo.title + '" ajoutée en base');
 					todos.push(this_todo);
