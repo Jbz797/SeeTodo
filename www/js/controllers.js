d8f7c7c694061ffe3c594c4436f82b7f3c5a29fc
root <jibusss50@gmail.com>
2016-05-24 23:36:06 +0200

    v0.7.32

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,11 +6,11 @@ angular.module('seetodo')
 	ionicMaterialInk.displayEffect();
 })
 
-.controller('NavCtrl', function ($scope, $ionicSideMenuDelegate, $state) {
+.controller('NavCtrl', function ($rootScope,$scope, $ionicSideMenuDelegate, $state) {
 	$scope.showMenu = function () {
 		$ionicSideMenuDelegate.toggleLeft();
 	};
-	$scope.url = $state.current.url;
+	$rootScope.url = $state.current.url;
 })
 
 .controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, $scope, storage) {
