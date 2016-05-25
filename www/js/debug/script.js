ff319c35e26ee289a81a59ff4a80638ec555b46a
root <jibusss50@gmail.com>
2016-05-25 17:47:48 +0200

    v0.8.74

--- www/js/debug/script.js
+++ www/js/debug/script.js
@@ -7651,62 +7651,58 @@ else if (typeof define === 'function' && define.amd) {
 
 	angular.module('seetodo')
 
-	.controller('AppCtrl', function (ionicMaterialInk) {
+	.controller('AppCtrl', function (ionicMaterialInk, $scope) {
 		ionicMaterialInk.displayEffect(); // Actionne les effets de vague sur certains éléments
 		if(ionic.Platform.isIOS()) { // Vérifie si l'on se trouve sur une plateforme ios
-			vm.is_ios = true;
+			$scope.is_ios = true;
 		}
 	})
 
-	.controller('NavCtrl', function ($ionicSideMenuDelegate) {
-
-		var vm = this;
+	.controller('NavCtrl', function ($scope, $ionicSideMenuDelegate) {
 
 		/**
 		 * @name ShowMenu
 		 * @desc Affiche le slide munu de gauche
 		 */
-		vm.showMenu = function () {
+		$scope.showMenu = function () {
 			$ionicSideMenuDelegate.toggleLeft();
 		};
 
 		/**
 		 * @desc Vérifie si le menu est ouvert
 		 */
-		vm.$watch(function () {
+		$scope.$watch(function () {
 			return $ionicSideMenuDelegate.isOpen();
 		}, function (value) {
-			vm.is_open = value;
+			$scope.is_open = value;
 		});
 
 		/**
 		 * @desc Vérifie l'url actuelle
 		 */
-		vm.$watch(function () {
+		$scope.$watch(function () {
 			return location.hash;
 		}, function (value) {
-			vm.url = value;
+			$scope.url = value;
 		});
 	})
 
-	.controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, storage) {
+	.controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, $scope, storage) {
 
-		var vm = this;
-
-		vm.newTodo = {};
-		vm.todos = storage.get_todos();
+		$scope.newTodo = {};
+		$scope.todos = storage.get_todos();
 		storage.get_database(); // On charge la base de donnée
 
 		/**
 		 * @name AddTodo
 		 * @desc Ajoute une tâche
 		 */
-		vm.addTodo = function () {
-			var newTodo = vm.newTodo;
+		$scope.addTodo = function () {
+			var newTodo = $scope.newTodo;
 			if(newTodo.title.length > 0) {
 				storage.add(newTodo)
 					.then(function success(response) {
-						vm.newTodo = {};
+						$scope.newTodo = {};
 					});
 			}
 		};
@@ -7716,7 +7712,7 @@ else if (typeof define === 'function' && define.amd) {
 		 * @desc Inverse l'état d'activation d'une tâche
 		 * @param {Object} la tâche à inverser
 		 */
-		vm.switchTodo = function (todo) {
+		$scope.switchTodo = function (todo) {
 			if(todo.title.length > 0) {
 				storage.switch(todo);
 			}
@@ -7727,7 +7723,7 @@ else if (typeof define === 'function' && define.amd) {
 		 * @desc Supprime une tâche
 		 * @param {Object} la tâche à supprimer
 		 */
-		vm.deleteTodo = function (todo) {
+		$scope.deleteTodo = function (todo) {
 			if(todo.title.length > 0) {
 				storage.delete(todo);
 			}
@@ -7738,7 +7734,7 @@ else if (typeof define === 'function' && define.amd) {
 		 * @desc Modifie une tâche
 		 * @param {Object} la tâche à modifier
 		 */
-		vm.editTodo = function (todo) {
+		$scope.editTodo = function (todo) {
 			if(todo.title.length > 0) {
 				storage.edit(todo);
 			}
@@ -7749,12 +7745,12 @@ else if (typeof define === 'function' && define.amd) {
 		 * @desc Affiche les details d'une tâche
 		 * @param {Object} la tâche concernée
 		 */
-		vm.showDetailsPopup = function (todo) {
-			vm.todo = todo;
+		$scope.showDetailsPopup = function (todo) {
+			$scope.todo = todo;
 			$ionicPopup.show({
 				templateUrl: 'templates/popup_details.html',
 				title: 'Details',
-				scope: vm,
+				scope: $scope,
 				buttons: [{
 					text: 'OK'
 				}]
@@ -7766,19 +7762,19 @@ else if (typeof define === 'function' && define.amd) {
 		 * @desc Affiche la popup d'édition d'une tâche
 		 * @param {Object} la tâche concernée
 		 */
-		vm.showEditPopup = function (todo) {
-			vm.todo = todo;
+		$scope.showEditPopup = function (todo) {
+			$scope.todo = todo;
 			$ionicPopup.show({
 				templateUrl: 'templates/popup_edit.html',
 				title: 'Modifier',
-				scope: vm,
+				scope: $scope,
 				buttons: [{
 					text: 'Annuler'
 				}, {
 					text: 'Sauvegarder',
 					type: 'button-balanced',
 					onTap: function () {
-						vm.editTodo(todo);
+						$scope.editTodo(todo);
 					}
 				}]
 			});
@@ -7788,7 +7784,7 @@ else if (typeof define === 'function' && define.amd) {
 		 * @name ShowConfirm
 		 * @desc Popup de confirmation avant suppression de la base
 		 */
-		vm.showConfirm = function () {
+		$scope.showConfirm = function () {
 			var confirmPopup = $ionicPopup.confirm({
 				cancelText: 'Annuler',
 				okType: 'button button-small button-balanced',
