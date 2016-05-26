6b68ea1db1569f89eaec5b3362f83222c4b84f19
root <jibusss50@gmail.com>
2016-05-26 13:05:53 +0200

    v0.9.15

--- www/js/debug/script.js
+++ www/js/debug/script.js
@@ -7687,7 +7687,30 @@ else if (typeof define === 'function' && define.amd) {
 		});
 	})
 
-	.controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, $scope, storage) {
+	.controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, $ionicSlides, $scope, storage) {
+
+		$scope.options = {
+			loop: false,
+			effect: 'fade',
+			speed: 500,
+		};
+
+		$scope.$on('$ionicSlides.sliderInitialized', function (event, data) {
+			// data.slider is the instance of Swiper
+			$scope.slider = data.slider;
+		});
+
+		$scope.$on('$ionicSlides.slideChangeStart', function (event, data) {
+			console.log('Slide change is beginning');
+		});
+
+		$scope.$on('$ionicSlides.slideChangeEnd', function (event, data) {
+			// note: the indexes are 0-based
+			$scope.activeIndex = data.activeIndex;
+			$scope.previousIndex = data.previousIndex;
+		});
+
+
 
 		$scope.newTodo = {};
 		$scope.todos = storage.getTodos();
