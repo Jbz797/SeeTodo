33fad1a77bb25276786e770294be3a8631e41463
root <jibusss50@gmail.com>
2016-05-26 20:23:15 +0200

    v1.0.1

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,39 +1,76 @@
 /*jshint -W020 */
 
-describe('Services', function () {
+(function () {
 
-	var $q;
-	var storage;
+	beforeEach(module('seetodo'));
 
-	var todoTest = {
-		activate: true,
-		color: false,
-		date: new Date(),
-		delete: false,
-		description: 'Description',
-		id: Math.floor(Math.random() * 100000) + 1,
-		title: 'Titre'
-	};
+	describe('Services', function () {
 
-	beforeEach(function () {
-		angular.mock.module('seetodo');
-		angular.mock.inject(function (_$q_, _storage_) {
+		var $q;
+		var storage;
+
+		var todoTest = {
+			activate: true,
+			color: false,
+			date: new Date(),
+			delete: false,
+			description: 'Description',
+			id: Math.floor(Math.random() * 100000) + 1,
+			title: 'Titre'
+		};
+
+		beforeEach(inject(function (_$q_, _storage_) {
 			$q = _$q_;
 			storage = _storage_;
+		}));
+
+		// Add
+		it('La méthode "add" existe', function () {
+			expect(storage.add)
+				.to.be.a('function');
+		});
+		it('La méthode "add" doit enregistrer une todo en base', function () {
+			storage.add(todoTest);
+			storage.getTodo(todoTest)
+				.then(function (response) {
+					console.log(response);
+				});
 		});
 	});
 
-	// Add
-	it('La méthode "add" existe', function () {
-		expect(storage.add)
-			.to.be.a('function');
-	});
-	it('La méthode "add" doit enregistrer une todo en base', function () {
-		var q = $q.defer();
-		storage.add(todoTest)
-			.then(function (result) {
-				q.resolve(result);
+	/*	describe('Services', function () {
+
+			var $q;
+			var storage;
+
+			var todoTest = {
+				activate: true,
+				color: false,
+				date: new Date(),
+				delete: false,
+				description: 'Description',
+				id: Math.floor(Math.random() * 100000) + 1,
+				title: 'Titre'
+			};
+
+			beforeEach(function () {
+				angular.mock.module('seetodo');
+				angular.mock.inject(function (_$q_, _storage_) {
+					$q = _$q_;
+					storage = _storage_;
+				});
 			});
-		console.log(q.promise);
-	});
-});
+
+			// Add
+			it('La méthode "add" existe', function () {
+				expect(storage.add)
+					.to.be.a('function');
+			});
+			it('La méthode "add" doit enregistrer une todo en base', function () {
+				storage.add(todoTest)
+					.then(function success(response) {
+						console.log(response);
+					});
+			});
+		});*/
+})();
