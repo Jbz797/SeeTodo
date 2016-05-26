0e244c2740719b74a67a879ddf7a743d2113c623
root <jibusss50@gmail.com>
2016-05-26 13:48:27 +0200

    v0.9.18

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,29 +42,6 @@
 
 	.controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, $scope, storage) {
 
-		$scope.options = {
-			loop: false,
-			effect: 'fade',
-			speed: 500,
-		};
-
-		$scope.$on('$ionicSlides.sliderInitialized', function (event, data) {
-			// data.slider is the instance of Swiper
-			$scope.slider = data.slider;
-		});
-
-		$scope.$on('$ionicSlides.slideChangeStart', function (event, data) {
-			console.log('Slide change is beginning');
-		});
-
-		$scope.$on('$ionicSlides.slideChangeEnd', function (event, data) {
-			// note: the indexes are 0-based
-			$scope.activeIndex = data.activeIndex;
-			$scope.previousIndex = data.previousIndex;
-		});
-
-
-
 		$scope.newTodo = {};
 		$scope.todos = storage.getTodos();
 		storage.getDatabase()
