902e15a8ed428455ccc0a8706ec04ac643de11b2
root <jibusss50@gmail.com>
2016-05-26 12:43:26 +0200

    v0.9.8

--- www/js/debug/script.js
+++ www/js/debug/script.js
@@ -7660,6 +7660,35 @@ else if (typeof define === 'function' && define.amd) {
 
 	.controller('NavCtrl', function ($scope, $ionicSideMenuDelegate) {
 
+		/**
+		 * @name ShowMenu
+		 * @desc Affiche le slide munu de gauche
+		 */
+		$scope.showMenu = function () {
+			$ionicSideMenuDelegate.toggleLeft();
+		};
+
+		/**
+		 * @desc Vérifie si le menu est ouvert
+		 */
+		$scope.$watch(function () {
+			return $ionicSideMenuDelegate.isOpen();
+		}, function (value) {
+			$scope.isOpen = value;
+		});
+
+		/**
+		 * @desc Vérifie l'url actuelle
+		 */
+		$scope.$watch(function () {
+			return location.hash;
+		}, function (value) {
+			$scope.url = value;
+		});
+	})
+
+	.controller('MainCtrl', function (ionicMaterialInk, $ionicPopup, $scope, storage) {
+
 	});
 })();
 
