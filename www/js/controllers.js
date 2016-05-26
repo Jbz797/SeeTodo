918d6c5731325cdb3de3f62d560c450d3c5e6a11
root <jibusss50@gmail.com>
2016-05-26 13:56:43 +0200

    v0.9.20

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,9 +6,6 @@
 
 	.controller('AppCtrl', function (ionicMaterialInk, $scope) {
 		ionicMaterialInk.displayEffect(); // Actionne les effets de vague sur certains éléments
-		if(ionic.Platform.isIOS()) { // Vérifie si l'on se trouve sur une plateforme ios
-			$scope.isIos = true;
-		}
 	})
 
 	.controller('NavCtrl', function ($scope, $ionicSideMenuDelegate) {
--- www/js/debug/script.js
+++ www/js/debug/script.js
@@ -3830,9 +3830,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	.controller('AppCtrl', function (ionicMaterialInk, $scope) {
 		ionicMaterialInk.displayEffect(); // Actionne les effets de vague sur certains éléments
-		if(ionic.Platform.isIOS()) { // Vérifie si l'on se trouve sur une plateforme ios
-			$scope.isIos = true;
-		}
 	})
 
 	.controller('NavCtrl', function ($scope, $ionicSideMenuDelegate) {
